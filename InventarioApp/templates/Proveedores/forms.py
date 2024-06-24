269c7299ec8abaa4b4a971b552c1e7aa471d63f6
benjaCeron <benjaceron1412@gmail.com>
2024-06-24 18:30:33 -0400

    .

--- InventarioApp/templates/Proveedores/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from InventarioApp.models import Proveedor
-
-class ProveedorForm(forms.ModelForm):
-    class Meta:
-        model = Proveedor
-        fields = ['nombre', 'direccion', 'telefono', 'email']
