50f19641e74b458059870f70e4706aff3581954a
benjaCeron <benjaceron1412@gmail.com>
2024-07-04 14:59:24 -0400

    Update settings.py

--- ProyectoInventario/settings.py
+++ ProyectoInventario/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Asegúrate de que esta línea esté presente
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Archivos de medios (subidas de usuarios)
 MEDIA_URL = '/media/'
